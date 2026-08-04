5696157b883a35333007984564cb3e7f7115e06f
xuyangli340-beep <xuyangli340@gmail.com>
2026-08-04 20:35:00 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import path from 'path';
 import { defineConfig } from '@lark-apaas/fullstack-vite-preset';
 
 export default defineConfig({
-  // 修正：末尾补上短横杠 -
   base: '/lab-of-love-/',
   resolve: {
     alias: {
